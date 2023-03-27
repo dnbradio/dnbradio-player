8ca69c0d70854cfae0c770ca8c3dea55fba33906
Mike <mike@promo.ly>
2023-03-27 02:26:50 -0600

    move rules from package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,22 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "@nuxtjs",
+    "plugin:nuxt/recommended",
+    "plugin:vue/essential",
+    "eslint:recommended"
   ],
-  // add your custom rules here
   rules: {
+    "no-console": "off",
+    "no-unused-vars": "off",
+    "vue/return-in-computed-property": "off",
+    "no-unreachable": "off",
+    "vue/no-unused-components": "off",
+    "space-before-function-paren": "off",
+    "vue/html-indent": "off",
+    "vue/multiline-html-element-content-newline": "off"
   }
-}
+};
